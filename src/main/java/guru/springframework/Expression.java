e13cf9de5bbe5e7ed6ef77a706b4b4c412350fba
Bhagyashree Chavan <bhagyashree.chavan90@gmail.com>
2020-09-08 12:21:59 +0530

    TDD by Example
    Test Driven Development by Example

---
+++ src/main/java/guru/springframework/Expression.java
@@ -0,0 +1,9 @@
+package guru.springframework;
+
+public interface Expression {
+    Expression plus(Expression addend);
+
+    Money reduce(Bank bank, String toCurrency);
+
+    Expression times(int multiplier);
+}
---
+++ src/main/java/guru/springframework/Sum.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+public class Sum implements Expression{
+    Expression augmend;
+    Expression addmend;
+
+    public Sum(Expression augmend, Expression addmend) {
+        this.augmend = augmend;
+        this.addmend = addmend;
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this,addend);
+    }
+
+    @Override
+    public Money reduce(Bank bank,String toCurrency){
+        int amount = augmend.reduce(bank,toCurrency).amount + addmend.reduce(bank,toCurrency).amount;
+        return new Money(amount,toCurrency);
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Sum(augmend.times(multiplier),addmend.times(multiplier));
+    }
+}
